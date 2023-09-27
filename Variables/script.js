26a41d63aed3facdbdb201858012a111b5305348
camillegdn <145430933+camillegdn@users.noreply.github.com>
2023-09-27 11:44:45 +0200

    Add files via upload

---
+++ Variables/script.js
@@ -0,0 +1,48 @@
+"use a strict"
+
+//dire de quel type est chaque exercices 
+//entre : number, string et boolean
+
+let avengers = 5 //-> number
+console.log("il y a " + avengers + " " + "avengers" );
+
+let jules = "César" //-> string
+console.log( "l'empereur romain s'appelle Jules " + jules);
+
+let étoiles = 12000 //-> number
+console.log( "il y a " + étoiles + " étoiles dans l'univers");
+
+let faceid = true //-> boolean
+console.log(faceid);
+
+let prixordi = 1200 // -> number
+console.log( "l'ordi fait à la main coute " + prixordi + " euros");
+
+let nain1 = "joyeux" //-> string
+let nain2 = "grincheux" //-> STRING
+let nain3 = "timide" // -> string
+let nain4 = "simplet" // -> string
+let nain5 = "atchoum" // -> string
+let nain6 = "prof" //-> string 
+let nain7 = "dormeur" // -> string 
+// on pourrait aussi faire un tableau donc ça aurait été du type objet 
+
+
+
+
+console.log( "les 7 nains de blanche-neige sont " + nain1 + ", " + nain2 + ", " + nain3 + ", " + nain4 + ", " + nain5 + ", " + nain6 + " et " + nain7);
+
+let nains=["Joyeux","Grincheux","Timide","Simplet","Atchoum","Prof","Dormeur"];
+
+    console.log(nains);
+    
+/* la quantité de clones que peut faire naruto -> number 
+3 défaut à donner à l'embauche -> string
+le prénom de anne dans la flamme -> string -> tableau donc type objet
+un panier sur amazon -> number
+durée en années de la guerre de 100 ans -> number
+caractéristique de la bière -> string
+le nom du prochain président de la République -> string */
+
+const pseudo = "camgdn" //-> on ne peut pas changer la constante 
+console.log(pseudo);
